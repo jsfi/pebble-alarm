6399848e3df2b8d760132c2ba4ce34a4c5c65411
Martin Sachse <martin.sachse+jsfi@gmail.com>
2015-12-02 13:07:10 +0100

    added progress bar in background

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,42 @@ var Vector2 = require('vector2');
 var UI = require('ui');
 var Vibe = require('ui/vibe');
 
-var w = new UI.Window();
+var w = new UI.Window({
+    backgroundColor: 'white'
+});
+var width = 144;
+var height = 152;
 var start = 10;
+
+var timerRectStart = new Vector2(width, height);
+var timerRectStop = new Vector2(width, 0);
+var timerRect = new UI.Rect({
+    backgroundColor: 'black',
+    position: new Vector2(0,0),
+    size: timerRectStart
+});
 
 var timer = start;
 var timerRun = false;
+var timerBg = new UI.Circle({
+    position: new Vector2(71, 67),
+    radius: 20
+});
+w.add(timerBg);
+
 var timerText = new UI.Text({
+    color: 'black',
     text: timer,
     textAlign: 'center',
-    position: new Vector2(4, 62),
-    size: new Vector2(136, 28)
+    position: new Vector2(4, 51),
+    size: new Vector2(136, 34)
 });
 w.add(timerText);
+
+var success = new UI.Card({
+    title: 'Timer ended',
+    body: '\nYour task is ready'
+});
 
 w.show();
 
@@ -40,8 +64,26 @@ function toggleTimer() {
     if (timerRun) {
         stopTimer();
     } else {
-        countDown();
+        startTimer();
     }
+}
+
+function startTimer() {
+    countDown();
+
+    w.insert(0, timerRect);
+    timerRect.animate({ size: timerRectStop, easing: 'linear' }, timer * 1000);
+}
+
+function stopTimer() {
+    clearTimeout(timerRun);
+    timerRun = false;
+    w.remove(timerRect);
+    timerRect.size(timerRectStart);
+}
+
+function updateTimer() {
+    timerText.text(timer);
 }
 
 function countDown() {
@@ -51,28 +93,14 @@ function countDown() {
         updateTimer();
 
         if (timerRun && timer <= 0) {
-            timer = start;
             stopTimer();
             timerEnded();
+            timer = start;
+            updateTimer();
         }
     }, 1000);
 }
 
-function stopTimer() {
-    clearTimeout(timerRun);
-    timerRun = false;
-}
-
-function updateTimer() {
-    timerText.text(timer);
-}
-
 function timerEnded() {
-    var card = new UI.Card({
-        title: 'Timer ended',
-        body: '\nYour task is ready'
-    });
-    Vibe.vibrate();
-
-    card.show();
+    success.show();
 }
